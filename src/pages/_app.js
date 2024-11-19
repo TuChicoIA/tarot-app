c19f003ba4c62c6fd2539b7b99bf66b59b80acda
TuChicoIA <frantuchicoia@gmail.com>
2024-11-20 00:25:41 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+// src/pages/_app.js
+import '../styles/globals.css'  // AQUÍ es donde debe estar la importación
 import { Inter } from 'next/font/google'
 
 export default function App({ Component, pageProps }) {
