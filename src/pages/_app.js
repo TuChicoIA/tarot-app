71eafd4a15d77d64e6b6268b62dfce41f295696f
Francisco González Vicente <gon_z@ESLCINTAF08.zar.loc>
2024-11-20 10:05:40 +0100

    Add Speed Insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
-// src/pages/_app.js
-import '../styles/globals.css'  // AQUÍ es donde debe estar la importación
-import { Inter } from 'next/font/google'
+import '../styles/globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  )
 }
